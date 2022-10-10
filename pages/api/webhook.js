f3cb1d2ecbbc07246151be3ebaa24eb7d9d6b502
JunQu <junjv@outlook.com>
2022-10-10 10:22:39 +0800

    fix: fix vercel config

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,45 +1,29 @@
-import { createHmac } from "node:crypto";
-
-const WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRE
-
-const getRawBody = (req) => {
-  return new Promise((resolve) => {
-    let bodyChunks = [];
-
-    req.on("end", () => {
-      const rawBody = Buffer.concat(bodyChunks).toString("utf8");
-      resolve(rawBody);
-    });
-
-    req.on("data", (chunk) => bodyChunks.push(chunk));
-
-  });
-};
+import { createHmac } from 'crypto';
 
 export default async function handleWebhook(req, res) {
   // verify the webhook signature request against the
   // unmodified, unparsed body
   const body = await getRawBody(req);
   if (!body) {
-    res.status(400).send("Bad request (no body)");
+    res.status(400).send('Bad request (no body)');
     return;
   }
 
   const jsonBody = JSON.parse(body);
 
   // compute our signature from the raw body
-  const secret = WEBHOOK_SECRET;
-  const signature = req.headers["x-hub-signature-256"];
+  const secret = process.env.GITHUB_WEBHOOK_SECRET;
+  const signature = req.headers['x-hub-signature-256'];
   const computedSignature =
-    "sha256=" + createHmac("sha256", secret).update(body).digest("hex");
+    'sha256=' + createHmac('sha256', secret).update(body).digest('hex');
 
   if (computedSignature === signature) {
     console.log(
-      "event",
-      req.headers["x-github-event"],
-      "action",
+      'event',
+      req.headers['x-github-event'],
+      'action',
       jsonBody.action,
-      "issue",
+      'issue',
       jsonBody.issue?.title,
       jsonBody.issue?.number
     );
@@ -48,17 +32,32 @@ export default async function handleWebhook(req, res) {
 
     // issue opened or edited
     // comment created or edited
-    console.log("[Next.js] Revalidating /");
-    await res.revalidate("/");
+    console.log('[Next.js] Revalidating /');
+    await res.revalidate('/');
     if (issueNumber) {
       console.log(`[Next.js] Revalidating /${issueNumber}`);
       await res.revalidate(`/${issueNumber}`);
     }
 
-    return res.status(200).send("Success!");
+    return res.status(200).send('Success!');
   } else {
-    return res.status(403).send("Forbidden");
+    return res.status(403).send('Forbidden');
   }
 }
 
+function getRawBody(req) {
+  return new Promise((resolve, reject) => {
+    let bodyChunks = [];
+    req.on('end', () => {
+      const rawBody = Buffer.concat(bodyChunks).toString('utf8');
+      resolve(rawBody);
+    });
+    req.on('data', (chunk) => bodyChunks.push(chunk));
+  });
+}
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
