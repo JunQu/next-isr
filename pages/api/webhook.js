9ca68b5a028967a8322948439e51e91e717ddd67
JunQu <junjv@outlook.com>
2022-10-10 14:42:04 +0800

    fix: fix get revalidate

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,47 +1,47 @@
-import { createHmac, timingSafeEqual } from "node:crypto";
+import { createHmac, timingSafeEqual } from "crypto";
 
-const secretKey = process.env.GITHUB_WEBHOOK_SECRET;
-
-function compareSignatures(signature, rawBody) {
+function compareSignatures(signature, rawBody,secretKey) {
   const computedSignature = createHmac("sha256", secretKey)
     .update(rawBody)
     .digest("hex");
   const hexSignature = signature.slice("sha256=".length);
 
   const sig = Buffer.from(hexSignature, "utf8");
-  const digest = Buffer.from(computedSignature);
+  const digest = Buffer.from(computedSignature, 'utf8');
 
   return sig.length === digest.length && timingSafeEqual(digest, sig);
 };
 
- async function handleWebhook(req, res) {
+export default async function handleWebhook(req, res) {
   // verify the webhook signature request against the
   // unmodified, unparsed body
-   const body = await getRawBody(req);
+  const body = await getRawBody(req);
   if (!body) {
-    res.status(400).send("Bad request (no body)");
+    res.status(400).send('Bad request (no body)');
     return;
   }
 
-  // compute our signature from the raw body
-  const signature = req.headers["x-hub-signature-256"];
-   const jsonBody = JSON.parse(body);
+  const jsonBody = JSON.parse(body);
 
-  if (compareSignatures(signature, body)) {
-    const issueNumber =jsonBody.issue?.number;
+  // compute our signature from the raw body
+  const secretKey = process.env.GITHUB_WEBHOOK_SECRET;
+  const signature = req.headers['x-hub-signature-256'];
+
+  if (compareSignatures(signature,body,secretKey)) {
+    const issueNumber = jsonBody.issue?.number;
 
     // issue opened or edited
     // comment created or edited
-    console.log("[Next.js] Revalidating /");
-    await res.revalidate("/");
+    console.log('[Next.js] Revalidating /');
+    await res.revalidate('/');
     if (issueNumber) {
       console.log(`[Next.js] Revalidating /${issueNumber}`);
       await res.revalidate(`/${issueNumber}`);
     }
 
-    return res.status(200).send("Success!");
+    return res.status(200).send('Success!');
   } else {
-    return res.status(403).send("Forbidden");
+    return res.status(403).send('Forbidden');
   }
 }
 
@@ -55,3 +55,9 @@ function getRawBody(req) {
     req.on('data', (chunk) => bodyChunks.push(chunk));
   });
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
